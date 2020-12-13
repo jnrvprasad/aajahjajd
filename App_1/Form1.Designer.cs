2b8b11953d535e8a9e20f525e8f92c768ef46969
Prasad JNRV <prasad.jnrv@gmail.com>
2020-12-13 12:44:45 +0530

    Initial commit

---
+++ App_1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace App_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPWD = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPWD = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(312, 67);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(60, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "User Name";
+            // 
+            // lblPWD
+            // 
+            this.lblPWD.AutoSize = true;
+            this.lblPWD.Location = new System.Drawing.Point(319, 196);
+            this.lblPWD.Name = "lblPWD";
+            this.lblPWD.Size = new System.Drawing.Size(53, 13);
+            this.lblPWD.TabIndex = 2;
+            this.lblPWD.Text = "Password";
+            this.lblPWD.Click += new System.EventHandler(this.lblPWD_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(426, 67);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtPWD
+            // 
+            this.txtPWD.Location = new System.Drawing.Point(426, 167);
+            this.txtPWD.Name = "txtPWD";
+            this.txtPWD.Size = new System.Drawing.Size(100, 20);
+            this.txtPWD.TabIndex = 5;
+            this.txtPWD.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPWD);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblPWD);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPWD;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPWD;
+    }
+}
+
---
+++ App_1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtName.Text == "Test" && txtPWD.Text=="Test")
+            {
+                MessageBox.Show("User Available");
+            }
+            else
+            {
+                MessageBox.Show("User Not Available");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPWD_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
